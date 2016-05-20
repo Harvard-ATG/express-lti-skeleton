b71dd8e5ca9a69081302dd8f0fe478bd683668f4
JaZahn <jcleveng@fas.harvard.edu>
2016-05-19 20:01:27 -0500

    redirecting a valid launch to an index.jade

--- app.js
+++ app.js
@@ -16,19 +16,30 @@ var ssl_options = {
 var consumer_key = "key";
 var consumer_secret = "secret";
 
-app.post('/launch_lti', bodyParser.urlencoded({ extended: false }), function(req, res){
+app.post('/lti/launch', bodyParser.urlencoded({ extended: false }), function(req, res){
   var provider = new lti.Provider(consumer_key, consumer_secret);
   console.log(req.body);
   provider.valid_request(req, function(err, isValid) {
-    res.send("valid_request? " + isValid);
+    //res.send("valid_request? " + isValid);
+    if(isValid){
+      res.redirect('/index');
+      //res.render('index');
+    }
   });
+});
 
+app.get('/index', bodyParser.urlencoded({ extended: false }), function(req, res){
+  res.render('index.jade');
+});
+
+app.get('/lti/config', function(req, res){
+  res.send("config");
 });
 
 app.listen(3001, function(){
   console.log("http server started at port 3001");
 });
 
-//var server = https.createServer(ssl_options, app).listen(3001, function(){
-//  console.log("https server started at port 3001");
-//});
+var server = https.createServer(ssl_options, app).listen(3002, function(){
+  console.log("https server started at port 3002");
+});
